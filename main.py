114bae90d69fde520d00b13d0486f34759bcb6ad
Marta Sleboda <sleboda.m98@gmail.com>
2021-04-14 17:51:31 +0200

    update

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
+# main.py
+
 from fastapi import FastAPI
 
 app = FastAPI()
 
 @app.get("/")
-def root():
+def root_view():
     return {"message": "Hello World"}
+
+@app.get("/hello/{name}")
+async def hello_name_view(name: str):
+    return f"Hello {name}"
+
